c62747db4a7cd5c69cdf5f8f2a4ad73edb77ebf1
ReeVNaR <ranveerghorpade16@gmail.com>
2025-03-29 02:28:39 +0530

    Python

---
+++ Basic/simpleinterest.py
@@ -0,0 +1,6 @@
+p = float(input("Enter principal amount: "))
+r = float(input("Enter annual interest rate (%): "))
+t = float(input("Enter time in years: "))
+
+simple_interest = (p * r * t) / 100
+print("Simple Interest:", simple_interest)
