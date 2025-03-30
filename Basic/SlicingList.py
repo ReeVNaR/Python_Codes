94186d90a5af1053fdadb38778c8ca46b5a12291
ReeVNaR <ranveerghorpade16@gmail.com>
2025-03-31 01:42:14 +0530

    Python

---
+++ Basic/SlicingList.py
@@ -0,0 +1,10 @@
+numbers = [10, 20, 30, 40, 50]
+
+# Get first 3 elements
+print(numbers[:3])  # [10, 20, 30]
+
+# Get last 2 elements
+print(numbers[-2:])  # [40, 50]
+
+# Get elements with step
+print(numbers[::2])  # [10, 30, 50]
