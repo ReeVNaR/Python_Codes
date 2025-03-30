8d1303cffe10c30577736878c14da96b83482ea4
ReeVNaR <ranveerghorpade16@gmail.com>
2025-03-31 01:39:45 +0530

    Python

---
+++ Basic/ListElements.py
@@ -0,0 +1,9 @@
+fruits = ["apple", "banana", "cherry"]
+
+# Access by index (0-based indexing)
+print(fruits[0])  # apple
+print(fruits[1])  # banana
+
+# Negative Indexing (Last element is -1)
+print(fruits[-1])  # cherry
+print(fruits[-2])  # banana
