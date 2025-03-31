9c1da40e30688ffac6523e0379f82dbb7f317467
Ranveer Ghorpade <ranveerghorpade16@gmail.com>
2025-04-01 02:48:49 +0530

    Python

--- Basic/Lists/ListCreate.py
+++ Basic/Lists/ListCreate.py
@@ -8,3 +8,12 @@ mixed = [10, "hello", 3.5, True]
 print("Empty list:", my_list)
 print("List with numbers:", numbers)
 print("List with mixed data types:", mixed)
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# Accessing an element
+print(matrix[1][2])  # 6
