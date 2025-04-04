7e48a8d5fba9f06c8c61edec67a0267e65e4f054
Ranveer Ghorpade <ranveerghorpade16@gmail.com>
2025-04-04 23:02:40 +0530

    Python

--- Basic/Lists/ListCreate.py
+++ Basic/Lists/ListCreate.py
@@ -3,7 +3,7 @@ my_list = []
 
 # List with elements
 numbers = [1, 2, 3, 4, 5]
-mixed = [10, "hello", 3.5, True]
+mixed = [16, "hello", 7.9, True]
 
 print("Empty list:", my_list)
 print("List with numbers:", numbers)
