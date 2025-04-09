278526ddad263d7af5d3f0e91c0d71b3e6049f65
ReeVNaR <ranveerghorpade16@gmail.com>
2025-04-10 02:49:56 +0530

    Python

---
+++ Basic/Tuple/Basic.py
@@ -0,0 +1,12 @@
+# Create a tuple
+my_tuple = ("Ranveer", 21, "India")
+
+# Access elements
+print("Name:", my_tuple[0])
+print("Age:", my_tuple[1])
+print("Country:", my_tuple[2])
+
+# Loop through tuple
+print("\nLooping through the tuple:")
+for item in my_tuple:
+    print(item)
