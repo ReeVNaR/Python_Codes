f15464ffb80d883df86434ad3bbc1683e7c4bc8a
ReeVNaR <ranveerghorpade16@gmail.com>
2025-04-10 02:52:58 +0530

    Python

---
+++ Basic/Tuple/SortStudents.py
@@ -0,0 +1,9 @@
+# List of students as (name, marks)
+students = [("Ranveer", 88), ("Pratiksha", 92), ("Aarav", 75), ("Sia", 85)]
+
+# Sort by marks (second item in tuple)
+sorted_students = sorted(students, key=lambda x: x[1], reverse=True)
+
+print("Students sorted by marks (high to low):")
+for name, marks in sorted_students:
+    print(name, "->", marks)
