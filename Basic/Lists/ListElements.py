679998724ad2408f9b0fa414c27c6326c87c4227
ReeVNaR <ranveerghorpade16@gmail.com>
2025-05-16 21:46:36 +0530

    Python

--- Basic/Lists/ListElements.py
+++ Basic/Lists/ListElements.py
@@ -4,7 +4,7 @@ fruits = ["apple", "banana", "cherry"]
 print(fruits[0])  # apple
 print(fruits[1])  # banana
 
-# Negative Indexing (Last element is -1)
+
 print(fruits[-1])  # cherry
 print(fruits[-2])  # banana
 
